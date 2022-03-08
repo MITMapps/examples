67c0827283e124b3a634826ce0ce0a7f523cc283
Matthew Thomson <m7homson@gmail.com>
2022-03-08 13:27:52 -0500

    changing wikipedia war -> peace

---
+++ flowers.py
@@ -0,0 +1,23 @@
+from mitmproxy import http
+from mitmproxy import ctx
+import json
+
+
+class Peace:
+    def __init__(self):
+        self.num = 0
+
+    def response(self, flow: http.HTTPFlow):
+        ctx.log.info("Received Response")
+        if 'wikipedia.org' in flow.request.url:
+            ctx.log.info(f'Received Response url {flow.request.url}')
+            ctx.log.info(f'Received Response headers {json.dumps(list(flow.response.headers.keys()))}')
+            if 'content-type' in flow.response.headers:
+                contenttype = flow.response.headers['content-type']
+                ctx.log.info(f'Received Response content-type {contenttype}')
+                if 'text/html' in contenttype:
+                    flow.response.text = flow.response.text.replace('nuclear', 'flowers')
+
+addons = [
+    Peace()
+]
